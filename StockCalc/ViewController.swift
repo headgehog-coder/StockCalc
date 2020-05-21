d8f2ccb7be3bcea6ccd505436b29475aece47b0b
Iwamoto Yoshiki <hosomi6930@icloud.com>
2020-05-21 12:07:51 +0900

    Initial Commit

---
+++ StockCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StockCalc
+//
+//  Created by 岩元喜輝 on 2020/05/21.
+//  Copyright © 2020 Yoshiki Iwamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
