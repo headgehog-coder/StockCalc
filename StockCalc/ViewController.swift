3ea46b567f479ee18995a4f13422c8b8cc53ba71
Iwamoto Yoshiki <hosomi6930@icloud.com>
2020-05-21 13:13:31 +0900

    パーツ連結

--- StockCalc/ViewController.swift
+++ StockCalc/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var priceTextField: UITextField!
+    @IBOutlet weak var numberOfStuckTextField: UITextField!
+    @IBOutlet weak var earningTextField: UITextField!
+    @IBOutlet weak var assetTextField: UITextField!
+    @IBOutlet weak var equityTextField: UITextField!
+    
+    @IBOutlet weak var resultButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
