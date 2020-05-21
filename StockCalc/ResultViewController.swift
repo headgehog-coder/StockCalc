7379782a54a1784f4e764c0f1eb954a2a74c16ad
Iwamoto Yoshiki <hosomi6930@icloud.com>
2020-05-21 14:27:15 +0900

    仮完成

--- StockCalc/ResultViewController.swift
+++ StockCalc/ResultViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
+    //アラート表示　宣言
+    var alertController: UIAlertController!
+    var price:Double = 0.0
+    var stock:Double = 0.0
+    var earning:Double = 0.0
+    var asset:Double = 0.0
+    var equity:Double = 0.0
+    @IBOutlet weak var perLabel: UILabel!
+    @IBOutlet weak var pbrLabel: UILabel!
+    @IBOutlet weak var roaLabel: UILabel!
+    @IBOutlet weak var roeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if price > 0 && stock > 0 && earning > 0 && asset > 0 && equity > 0{
+             let intPer = Int(price / (earning / stock))
+                   let intPbr = Int(price / (equity / stock))
+                   let intRoa = Int(earning / asset * 100)
+                   let intRoe = Int(earning / equity * 100)
+            perLabel.text = String(intPer)
+            pbrLabel.text = String(intPbr)
+            roaLabel.text = String(intRoa)
+            roeLabel.text = String(intRoe)
+        }else{
+            
+               alert(title: "エラー", message: "前画面にて0以上の数字を入力して下さい")
+             
+        }
+       // Do any additional setup after loading the view.
     }
+    
+     
+
+    //メソッド
+     func alert(title:String, message:String) {
+            alertController = UIAlertController(title: title,message: message,preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK",style: .default,handler: nil))
+            present(alertController, animated: true)
+    }
+
+  @IBAction func backButton(_ sender: Any) {
+                   self.dismiss(animated: true, completion: nil)
+               }
+    
     
 
     /*
--- StockCalc/ViewController.swift
+++ StockCalc/ViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
-    
+    //アラート表示　宣言
+    var alertController: UIAlertController!
     @IBOutlet weak var priceTextField: UITextField!
     @IBOutlet weak var numberOfStuckTextField: UITextField!
     @IBOutlet weak var earningTextField: UITextField!
@@ -20,9 +21,68 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+//        priceTextField.delegate = self
+//        numberOfStuckTextField.delegate = self
+//        earningTextField.delegate = self
+//        assetTextField.delegate = self
+        equityTextField.delegate = self
         // Do any additional setup after loading the view.
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        priceTextField.text = ""
+         numberOfStuckTextField.text = ""
+         earningTextField.text = ""
+         assetTextField.text = ""
+         equityTextField.text = ""
+    }
+     
 
+    //メソッド
+     func alert(title:String, message:String) {
+            alertController = UIAlertController(title: title,message: message,preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK",style: .default,handler: nil))
+            present(alertController, animated: true)
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        resultButton.sendActions(for: .touchUpInside)
+        return true
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+   
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if identifier == "result"{
+            guard priceTextField.text != "" && numberOfStuckTextField.text != "" && earningTextField.text != "" && assetTextField.text != "" && equityTextField.text != "" else{
+                    alert(title: "エラー", message: "入力して下さい")
+                return false
+            }
+             
+        }
+        return true
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let identifire = segue.identifier else{
+            return
+        }
+        if identifire == "result"{
+            let resultVC = segue.destination as! ResultViewController
+            resultVC.price = Double(priceTextField.text!)!
+            resultVC.stock = Double(numberOfStuckTextField.text!)!
+            resultVC.earning = Double(earningTextField.text!)!
+            resultVC.asset = Double(assetTextField.text!)!
+            resultVC.equity = Double(equityTextField.text!)!
+            self.priceTextField.resignFirstResponder()
+             self.numberOfStuckTextField.resignFirstResponder()
+             self.earningTextField.resignFirstResponder()
+             self.assetTextField.resignFirstResponder()
+             self.equityTextField.resignFirstResponder()
+        }
+    }
 
 }
 
